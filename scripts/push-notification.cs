c656bbb01a627e5c2ddaa0f4c0ecccae6e941ba7
Weihan Li <weihanli@outlook.com>
2024-01-10 01:14:30 +0800

    update push-notification script

--- scripts/push-notification.cs
+++ scripts/push-notification.cs
@@ -1,8 +1,5 @@
 using System.Net.Http.Json;
 
-var webhookUrl = Environment.GetEnvironmentVariable("DingBotWebhookUrl");
-if (string.IsNullOrEmpty(webhookUrl)) return;
-
 var messageTemplate = """
 The service {{env serviceName}} deploy with version {{env imageName}}
 Repo: {{env Build.Repository.Uri}}
@@ -11,6 +8,14 @@ Repo: {{env Build.Repository.Uri}}
 var message = await WeihanLi.Common.Template.TemplateEngine.CreateDefault()
     .RenderAsync(messageTemplate);
 Console.WriteLine(message);
+
+var webhookUrl = Environment.GetEnvironmentVariable("DingBotWebhookUrl");
+if (string.IsNullOrEmpty(webhookUrl))
+{
+    Console.WriteLine("WebhookUrl not found");
+    return;
+}
+
 using var response = await HttpHelper.HttpClient.PostAsJsonAsync(webhookUrl, new
     {
         msgtype = "text",
